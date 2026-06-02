efff406cf8354cb6bcf52e9e230940b7f81b94ae
BreakingLead <breaingleadqwq@gmail.com>
2026-06-02 12:35:27 +0800

    fix: use grass_block_top texture ID to match atlas filename

--- blockworld-server/src/block/mod.rs
+++ blockworld-server/src/block/mod.rs
@@ -1,18 +1,18 @@
 pub mod block;
 pub mod block_face_direction;
 pub use block::*;
-use blockworld_utils::Registry;
+use blockworld_utils::{Identifier, Registry};
 use once_cell::sync::Lazy;
 
 pub static BLOCK_REGISTRY: Lazy<Registry<Block>> = Lazy::new(|| {
     let mut r = Registry::new();
-    let a0 = Block::new(blockworld_utils::Identifier::new(&format!("{}:air", blockworld_utils::GAME_NAME)));
+    let a0 = Block::new(Identifier::new(&format!("{}:air", blockworld_utils::GAME_NAME)));
     r.register(a0);
-    let a1 = Block::new(blockworld_utils::Identifier::new(&format!("{}:stone", blockworld_utils::GAME_NAME)));
+    let a1 = Block::new(Identifier::new(&format!("{}:stone", blockworld_utils::GAME_NAME)));
     r.register(a1);
-    let a2 = Block::new(blockworld_utils::Identifier::new(&format!("{}:dirt", blockworld_utils::GAME_NAME)));
+    let a2 = Block::new(Identifier::new(&format!("{}:dirt", blockworld_utils::GAME_NAME)));
     r.register(a2);
-    let a3 = Block::new(blockworld_utils::Identifier::new(&format!("{}:grass_block", blockworld_utils::GAME_NAME)));
+    let a3 = Block::new(Identifier::new(&format!("{}:grass_block_top", blockworld_utils::GAME_NAME)));
     r.register(a3);
 
     r
